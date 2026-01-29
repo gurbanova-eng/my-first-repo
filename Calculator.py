f67ea694fca7245fc6b573c3bb4f3fe5beccd578
gurbanova-eng <gurbanovaa69@gmail.com>
2026-01-29 20:57:53 +0500

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,17 @@
+# Мини-калькулятор
+a = 5
+b = 3
+
+# Сложение
+sum_result = a + b
+print("Сумма:", sum_result)
+
+# Умножение
+mul_result = a * b
+print("Произведение:", mul_result)
+
+# Проверка
+if sum_result > mul_result:
+    print("Сумма больше произведения")
+else:
+    print("Произведение больше или равно сумме")
